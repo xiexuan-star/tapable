c11bcd258fd2bd5f90cc99ed337878583335b6a7
xiexuan-star <xiexuan127@foxmail.com>
2022-04-17 21:22:41 +0800

    feat: all hook demo complete

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-import { SyncBailHook, SyncHook, SyncLoopHook, SyncWaterfallHook } from 'tapable';
+import {
+  AsyncParallelBailHook,
+  AsyncParallelHook,
+  AsyncSeriesBailHook, AsyncSeriesHook, AsyncSeriesWaterfallHook, SyncBailHook, SyncHook, SyncLoopHook,
+  SyncWaterfallHook
+} from 'tapable';
 
+// ------------------------------------ 同步Hook ---------------------------------
 // 最常规的钩子
 const syncHook = new SyncHook(['name']);
 syncHook.tap('car', (name) => {
@@ -36,7 +42,7 @@ syncWaterfallHook.tap('car', name => {
 });
 syncWaterfallHook.call('motorbike');
 
-// 循环Hook，当有返回值时，所有hook重新执行知道所有hook都没有返回值
+// 循环Hook，当有返回值时，所有hook重新执行直到所有hook都没有返回值
 const syncLoopHook = new SyncLoopHook(['name']);
 syncLoopHook.tap('car', () => {
   if (Math.random() > 0.5) return 1;
@@ -51,3 +57,129 @@ syncLoopHook.tap('car', () => {
   console.log(3);
 });
 syncLoopHook.call(1);
+
+// ------------------------------------ 异步Hook ---------------------------------
+const asyncSeriesHook = new AsyncSeriesHook(['name']);
+// callback采用的时err-first风格的错误机制
+asyncSeriesHook.tapAsync('car', (name, callback) => {
+  setTimeout(() => {
+    console.log(name + '1');
+    callback();
+  }, 100);
+});
+asyncSeriesHook.tapAsync('car', (name, callback) => {
+  setTimeout(() => {
+    console.log(name + '2');
+    callback();
+  }, 100);
+});
+asyncSeriesHook.tapPromise('car', name => {
+  return new Promise<any>(resolve => {
+    setTimeout(() => {
+      resolve(name);
+    }, 500);
+  });
+});
+console.time('asyncSeriesHook');
+asyncSeriesHook.callAsync('ambulance', () => {
+  console.timeEnd('asyncSeriesHook');
+});
+
+const asyncSeriesBailHook = new AsyncSeriesBailHook(['name']);
+// 同asyncSeriesHook,但当hook返回了非空值时,会中断后续钩子执行
+asyncSeriesBailHook.tapAsync('car', (name, callback) => {
+  setTimeout(() => {
+    console.log(name + '1');
+    callback(null);
+  }, 100);
+});
+asyncSeriesBailHook.tapPromise('car', name => {
+  return new Promise<any>(resolve => {
+    setTimeout(() => {
+      console.log(name + '2');
+      resolve(name);
+    }, 500);
+  });
+});
+asyncSeriesBailHook.tapPromise('car', name => {
+  return new Promise<any>(resolve => {
+    setTimeout(() => {
+      console.log(name + '3');
+      resolve(name);
+    }, 500);
+  });
+});
+console.time('asyncSeriesBailHook');
+asyncSeriesBailHook.callAsync('truck', () => {
+  console.timeEnd('asyncSeriesBailHook');
+});
+
+const asyncSeriesWaterfallHook = new AsyncSeriesWaterfallHook(['name']);
+asyncSeriesWaterfallHook.tapAsync('car', (name, callback) => {
+  setTimeout(() => {
+    console.log(name + '1');
+    callback(null, name + '1');
+  }, 100);
+});
+asyncSeriesWaterfallHook.tapPromise('car', name => {
+  return new Promise<any>(resolve => {
+    setTimeout(() => {
+      console.log(name + '2');
+      resolve(name);
+    }, 500);
+  });
+});
+console.time('asyncSeriesWaterfallHook');
+asyncSeriesWaterfallHook.promise('tank').then(() => {
+  console.timeEnd('asyncSeriesWaterfallHook');
+});
+
+const asyncParallelHook = new AsyncParallelHook(['name']);
+asyncParallelHook.tapPromise('car', name => new Promise<void>(
+  resolve => {
+    setTimeout(() => {
+      console.log(name + '1');
+      resolve();
+    }, 510);
+  }
+));
+asyncParallelHook.tapPromise('car', name => new Promise<void>(
+  resolve => {
+    setTimeout(() => {
+      console.log(name + '2');
+      resolve();
+    }, 500);
+  }
+));
+
+console.time('asyncParallelHook');
+asyncParallelHook.promise('airplane').then(() => {
+  console.timeEnd('asyncParallelHook');
+});
+
+const asyncParallelBailHook = new AsyncParallelBailHook(['name']);
+asyncParallelBailHook.tapPromise('car', name => new Promise(
+  resolve => {
+    setTimeout(() => {
+      console.log(name + '1');
+      resolve(true);
+    }, 510);
+  }
+));
+asyncParallelBailHook.tapPromise('car', name => new Promise<void>(
+  resolve => {
+    setTimeout(() => {
+      console.log(name + '2');
+      resolve();
+    }, 1000);
+  }
+));
+
+console.time('asyncParallelBailHook');
+asyncParallelBailHook.promise('bullet train').then(() => {
+  console.timeEnd('asyncParallelBailHook');
+});
+
+// ------------------------------------ interceptor ---------------------------------
+
+
